eacea5a43b91b994294150b965f0f4a99bd73285
Mohamed Elatabany <mmemo4855@gmail.com>
2018-06-12 05:14:35 +0200

    add item fucntionalty complete

--- Todoey/ToDoListVC.swift
+++ Todoey/ToDoListVC.swift
@@ -9,16 +9,44 @@
 import UIKit
 
 class ToDoListVC: UITableViewController {
-
     
-    let itemArray = ["Learn some swift", "read my brand new books", "work on the projects"]
+    var itemArray = ["Learn some swift", "read my brand new books", "work on the projects"]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
-    //MARK: - Tablview Datasource Methods
+    @IBAction func addButtonAction(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen one the user clicks the add item button on our uialert
+            if let text = textField.text, !text.isEmpty {
+                self.itemArray.append(text)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            // triggered when the textfield added to the alert
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            textField.addTarget(self, action: #selector(self.textChanged(sender:)), for: .editingChanged)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        self.actionToEnable = action
+        action.isEnabled = false
+        present(alert, animated: true)
+    }
     
+    weak var actionToEnable : UIAlertAction?
+    @objc func textChanged(sender:UITextField) {
+        self.actionToEnable?.isEnabled = (sender.text! != "")
+    }
+    
+    
+    //MARK: - Tablview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -29,29 +57,10 @@ class ToDoListVC: UITableViewController {
         return cell
     }
     
-    
-//    var dict: [Int: Bool] = [:]
-    
     //MARK: - Tablview Delgate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-//        if dict[indexPath.row] == nil {
-//            dict[indexPath.row] = true
-//        } else {
-//            dict[indexPath.row] = !dict[indexPath.row]!
-//        }
-//
-//        if dict[indexPath.row] == true {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        }
-//
-        
          tableView.cellForRow(at: indexPath)?.accessoryType = (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark ) ? .none : .checkmark
-        
     }
-    
-
 }
 
